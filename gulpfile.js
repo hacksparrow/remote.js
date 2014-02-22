9f14953dd317c245ffadd727bfaae5bf3b420119
Hage Yaapa <captain@hacksparrow.com>
2014-02-22 08:29:40 +0530

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,28 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 gulp.task('build', function() {
-  gulp.run('transmitter', 'receiver');
-  gulp.watch(['dist/*.js'], function(event) {
-    gulp.src(['dist/*.js']).pipe(gulp.dest('example/'));
-  });
+
 });
 
 gulp.task('transmitter', function() {
-  gulp.src(['src/remote.js', 'src/transmitter.js'])
+  return gulp.src(['src/remote.js', 'src/transmitter.js'])
     .pipe(concat('remote.transmitter.js'))
     //.pipe(uglify())
     .pipe(gulp.dest('./dist/'))
 });
 
 gulp.task('receiver', function() {
-  gulp.src(['src/remote.js', 'src/receiver.js'])
+  return gulp.src(['src/remote.js', 'src/receiver.js'])
     .pipe(concat('remote.receiver.js'))
     //.pipe(uglify())
     .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('default', function() {
-  gulp.run('build');
-  gulp.watch(['src/*.js'], function(event) {
-    gulp.run('build');
+gulp.task('watch', function() {
+  gulp.watch(['src/*.js'], ['transmitter', 'receiver']);
+  gulp.watch(['dist/*.js'], function() {
+    return gulp.src(['dist/*.js']).pipe(gulp.dest('examples/'));
   });
 });
+
+gulp.task('default', ['transmitter', 'receiver', 'watch']);
